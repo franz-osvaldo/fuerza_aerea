10ad2702c4a17737d01ab51c454011f854e2f909
Franz Beltran <franzbeltran79@gmail.com>
2016-06-29 06:59:04 -0400

    Implementar barras lateral y superior de navegacion

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,40 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+$(document).on('page:change', function() {
+    function setCookie(c_name, value){
+        document.cookie  =  c_name + " = " + value + "; path=/";
+    }
+    function deleteCookie(c_name) {
+        document.cookie = c_name + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    }
+    function wasCookieSet(cookie_name) {
+        var array_of_cookies = document.cookie.split(';');
+        for (var i = 0; i < array_of_cookies.length; i++){
+            if (array_of_cookies[i].indexOf(cookie_name) > -1) {
+                return true
+            }
+        }
+        return false
+    }
+    function setInitialState() {
+        if (wasCookieSet('sidebar_state')){
+            $('.side_navbar').css('marginLeft', '-250px');
+            $('.main_container').css('marginLeft', '0');
+        }
+    }
+    setInitialState();
+
+    $('.sidebar_button').click(function () {
+        if (wasCookieSet('sidebar_state')){
+            deleteCookie('sidebar_state');
+            $('.side_navbar').animate({marginLeft: "0"});
+            $('.main_container').animate({marginLeft: "250px"});
+        }
+        else{
+            setCookie('sidebar_state', 'toggled');
+            $('.side_navbar').animate({marginLeft: "-250px"});
+            $('.main_container').animate({marginLeft: "0"});
+        }
+    });
+});
